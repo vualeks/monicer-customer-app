cec6d19e83d6e69a270478068e8fc51c1c9bb3aa
Aleksandar Vukovic <vualeks@gmail.com>
2018-10-12 06:47:54 +0200

    Added new server URL

--- app/src/main/java/me/hackathon/monicercustomerapp/util/Constants.kt
+++ app/src/main/java/me/hackathon/monicercustomerapp/util/Constants.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import me.hackathon.monicercustomerapp.vo.User
 
 object Constants {
-    const val BASE_URL = "http://192.168.2.117:8080/"
+    const val BASE_URL = "http://178.128.195.254:8080/"
 
     lateinit var CURRENT_USER: User
 
